f3fccf24d9444f1ec0624f9cd8bcb0cd076edcb2
hellonasd <hellonasd@gmail.com>
2020-11-26 15:54:23 +0300

    'bad'

--- src/components/viewall/Viewall.jsx
+++ src/components/viewall/Viewall.jsx
@@ -3,7 +3,7 @@ import ViewallStyle from '../viewall/viewall.module.css';
 export const Viewall = () => {
   return (
     <Fragment>
-      <a className={ViewallStyle.link} href='#'>View all</a>
+      <button className={ViewallStyle.link}>View all</button>
     </Fragment>
   )
 }
