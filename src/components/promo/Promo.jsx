bdbe7b4accb3ceac79f3d875f5ff14efb3862c82
hellonasd <hellonasd@gmail.com>
2020-11-27 19:32:55 +0300

    'promov1'

--- src/components/promo/Promo.jsx
+++ src/components/promo/Promo.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import { PromoBtn } from '../promoBtn/PromoBtn.jsx';
+import { PromoBtn } from "../promoBtn/PromoBtn.jsx";
 
-import PromoStyle from '../promo/Promo.module.css';
-
+import PromoStyle from "../promo/Promo.module.css";
 
 export const Promo = () => {
   return (
     <div className={PromoStyle.promo__wrapper}>
-      <div className={PromoStyle.promo__title}>Street fashion collection</div>
-      <p className={PromoStyle.promo__subtitle}>Fall 2019</p>
-      <PromoBtn btnSubstring='Shop now'/>
+      <div className={PromoStyle.promoBg}>
+        <div className={PromoStyle.promo__title}>Street fashion collection</div>
+        <p className={PromoStyle.promo__subtitle}>Fall 2019</p>
+        <PromoBtn btnSubstring="Shop now" />
+      </div>
     </div>
-  )
-}
+  );
+};
