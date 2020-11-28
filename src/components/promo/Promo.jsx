33d0bebfd5f57aef071eeb2e08f686d039a67a4f
hellonasd <hellonasd@gmail.com>
2020-11-28 17:14:55 +0300

    'promoalpha'

--- src/components/promo/Promo.jsx
+++ src/components/promo/Promo.jsx
@@ -9,12 +9,25 @@ export const Promo = () => {
     <div className={PromoStyle.promo__wrapper}>
       <div className={PromoStyle.promoBg}>
         <picture>
-          <source srcSet='../../img/new-collection-mobile.jpg' media='(max-width: 768px)'/>
-          <img className={PromoStyle.promo__img} src="../../img/new-collection-tablet.jpg" alt=""/>
+          <source
+            className={PromoStyle.promo__mobImg}
+            srcSet="../../img/new-collection-mobile.jpg"
+            media="(max-width: 768px)"
+          />
+          <img
+            className={PromoStyle.promo__img}
+            src="../../img/new-collection-tablet.jpg"
+            alt=""
+          />
         </picture>
-        <div className={PromoStyle.promo__title}>Street fashion collection</div>
-        <p className={PromoStyle.promo__subtitle}>Fall 2019</p>
-        <PromoBtn btnSubstring="Shop now" />
+        <div className={PromoStyle.promo__wrapper__title}>
+          <div className={PromoStyle.promo__title}>
+            Street fashion collection
+          </div>
+          <p className={PromoStyle.promo__subtitle}>Fall 2019</p>
+          <PromoBtn btnSubstring="Shop now" />
+        </div>
+        {/* <PromoBtn btnSubstring="Shop now" /> */}
       </div>
     </div>
   );
