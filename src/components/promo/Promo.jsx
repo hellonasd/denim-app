89942ff6ec7833dde584dd60ececddc34d519f98
hellonasd <hellonasd@gmail.com>
2020-12-03 20:44:05 +0300

    promo adaptive 768px

--- src/components/promo/Promo.jsx
+++ src/components/promo/Promo.jsx
@@ -7,12 +7,11 @@ export const Promo = () => {
   return (
     <div className="promo__main">
       <div className="promo__wrapper">
-        <div className="promo__img--wrapper">
-          <picture>
-            <source srcSet={promoTemp.url.tablet} media="(min-width: 768px)" />
-            <img className="promo__img" src={promoTemp.url.mobile} alt="" />
-          </picture>
-        </div>
+        <picture>
+          <source srcSet={promoTemp.url.tablet} media="(min-width: 768px)" />
+          <img className="promo__img" src={promoTemp.url.mobile} alt="" />
+        </picture>
+
         <div className="promo__collection">
           <div className="promo__background">
             <p className="promo__description">{promoTemp.description}</p>
