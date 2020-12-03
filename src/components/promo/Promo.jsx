c69b069c23c397ad40fdfd2eb57ff9260d59296d
hellonasd <hellonasd@gmail.com>
2020-12-03 14:22:31 +0300

    add promo mobile v1

--- src/components/promo/Promo.jsx
+++ src/components/promo/Promo.jsx
@@ -1,10 +1,26 @@
 import React from "react";
-
-
+import { Btn } from "../Btn/Btn.jsx";
 import { promoTemp } from "../../moco-data/mocoData.js";
-
-
+import "../promo/Promo.css";
 
 export const Promo = () => {
-  <div></div>
+  return (
+    <div className="promo__main">
+      <div className="promo__wrapper">
+        <div className="promo__img--wrapper">
+          <picture>
+            <source srcSet={promoTemp.url.tablet} media="(min-width: 768px)" />
+            <img className="promo__img" src={promoTemp.url.mobile} alt="" />
+          </picture>
+        </div>
+        <div className="promo__collection">
+          <div className="promo__background">
+            <p className="promo__description">{promoTemp.description}</p>
+            <h1 className="promo__title">{promoTemp.title}</h1>
+            <Btn subClass="btn--promo" btnSubstring="Shop now" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
