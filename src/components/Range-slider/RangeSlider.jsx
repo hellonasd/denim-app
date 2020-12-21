ac5272529946e7bf1dc6fad02ae876ee644da9a5
hellonasd <hellonasd@gmail.com>
2020-12-21 19:46:49 +0300

    bad slider mb end

--- src/components/Range-slider/RangeSlider.jsx
+++ src/components/Range-slider/RangeSlider.jsx
@@ -2,20 +2,26 @@ import React from "react";
 import "../Range-slider/rangerSlider.css";
 
 export const RangerSlider = () => {
-  const [valueLeft, setValueLeft] = React.useState(25);
-  const [valueRight, setValueRight] = React.useState(75);
+  const [valueLeft, setValueLeft] = React.useState(40);
+  const [valueRight, setValueRight] = React.useState(240);
  
   const left = React.useRef(null);
   const right = React.useRef(null);
-
+  const range = React.useRef(null);
   const setLeft = (event) => {
     
     let right = event.target.nextSibling;
+    console.log(right.value);
+    console.log(event.target.min);
+    console.log(event.target.max);
     let min = event.target.min;
     let max = event.target.max;
     let value = Math.min(parseInt(event.target.value), parseInt(right.value) - 1);
     let percent = ((value - min) / (max - min)) * 100;
     left.current.style.left = percent + '%';
+    range.current.style.left = percent + '%';
+   
+    
   }
 
 
@@ -27,7 +33,7 @@ export const RangerSlider = () => {
     let percent = ((value - min) / (max - min)) * 100;
     console.log(value)
     right.current.style.right = (100 - percent) + '%';
-
+    range.current.style.right = (100 - percent) + '%';
   }
   return (
     <div className="middle">
@@ -36,8 +42,8 @@ export const RangerSlider = () => {
           type="range"
           name="one"
           id="input-left"
-          min="0"
-          max="100"
+          min='0'
+          max='240'
           value={valueLeft}
           onInput={(event)=>setLeft(event)}
         />
@@ -45,14 +51,14 @@ export const RangerSlider = () => {
           type="range"
           name="one"
           id="input-right"
-          min="0"
-          max="100"
+          min='0'
+          max='240'
           value={valueRight}
           onInput={event => setRight(event)}
         />
         <div className="slider">
           <div className="track"></div>
-          <div className="range"></div>
+          <div ref={range} className="range"></div>
           <div ref={left} className="thumb left"></div>
           <div ref={right} className="thumb right"></div>
         </div>
