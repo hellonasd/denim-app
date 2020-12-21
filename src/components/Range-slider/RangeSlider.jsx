f532702b9b4d46b5d0c9db4ffcc77467d6c22dec
hellonasd <hellonasd@gmail.com>
2020-12-21 16:46:14 +0300

    rebild range slider

--- src/components/Range-slider/RangeSlider.jsx
+++ src/components/Range-slider/RangeSlider.jsx
@@ -2,66 +2,59 @@ import React from "react";
 import "../Range-slider/rangerSlider.css";
 
 export const RangerSlider = () => {
-  const [valueLeft, setValueLeft] = React.useState(0);
-  const [valueRight, setValueRight] = React.useState(45);
-  
-  
-  const thumbLeftRef = React.useRef(null);
-  const thumbRightRef = React.useRef(null);
-  const thumbRangeRef = React.useRef(null);
-  const inputLeftRef = React.useRef(null);
-  const inputRightRef = React.useRef(null);
-  
-  React.useEffect(() => {
-    
-    
-  }, [thumbLeftRef, inputLeftRef,thumbRightRef,thumbRangeRef,inputRightRef]);
+  const [valueLeft, setValueLeft] = React.useState(25);
+  const [valueRight, setValueRight] = React.useState(75);
+ 
+  const left = React.useRef(null);
+  const right = React.useRef(null);
 
-  const setLeft = () => {
+  const setLeft = (event) => {
     
-    let min = +inputLeftRef.current.min;
-    let max = +inputLeftRef.current.max;
-    let value = Math.min(+inputLeftRef.current.value, +inputRightRef.current.value - 1);
-    let precent = (value - min) / (max - min) * 100;
-    console.log(thumbLeftRef.current.style.left)
-    
-    setValueLeft(thumbLeftRef.current.style.left = precent + '%');
+    let right = event.target.nextSibling;
+    let min = event.target.min;
+    let max = event.target.max;
+    let value = Math.min(parseInt(event.target.value), parseInt(right.value) - 1);
+    let percent = ((value - min) / (max - min)) * 100;
+    left.current.style.left = percent + '%';
   }
 
-  const setRight = () => {
-    let min = +inputLeftRef.current.min;
-    let max = +inputLeftRef.current.max;
-    let value = Math.max(+inputLeftRef.current.value, +inputRightRef.current.value - 1);
-    setValueRight(value);
-  }
 
+  const setRight = (event) => {
+    let left = event.target.previousElementSibling;
+    let min = event.target.min;
+    let max = event.target.max;
+    let value = Math.max(parseInt(event.target.value), parseInt(left.value) + 1);
+    let percent = ((value - min) / (max - min)) * 100;
+    console.log(value)
+    right.current.style.right = (100 - percent) + '%';
+
+  }
   return (
     <div className="middle">
       <div className="multi-ranger-slider">
         <input
           type="range"
-          name=""
+          name="one"
           id="input-left"
           min="0"
           max="100"
-          ref={inputLeftRef}
-          onInput={setLeft}
           value={valueLeft}
+          onInput={(event)=>setLeft(event)}
         />
         <input
           type="range"
-          name=""
+          name="one"
           id="input-right"
           min="0"
           max="100"
-          ref={inputRightRef}
           value={valueRight}
+          onInput={event => setRight(event)}
         />
         <div className="slider">
           <div className="track"></div>
-          <div ref={thumbRangeRef} className="range"></div>
-          <div ref={thumbLeftRef} className="thumb left"></div>
-          <div ref={thumbRightRef} className="thumb right"></div>
+          <div className="range"></div>
+          <div ref={left} className="thumb left"></div>
+          <div ref={right} className="thumb right"></div>
         </div>
       </div>
     </div>
